3d04ee3cbebffbd8f5bc03f3208eaa57e46bc889
prgrm274 <programmer270487@gmail.com>
2020-02-21 09:04:55 +0700

    Jum, 21 Feb 2020  9.01.40,20

--- src/Cities.js
+++ src/Cities.js
@@ -1,25 +1,22 @@
-import React, {Component} from "react";
+import React from "react";
 
-class Cities extends Component {
-    render() {
-        // console.log(this.props);
-
-        // const {nama, luas, berpantai} = this.props;
-        const {cities} = this.props;
-        const cityList = cities.map(city => {
-            return (
-                <div className="city" key={city.id}>
-                    <div>Nama: {city.nama}</div>
-                    <div>Luas: {city.luas}</div>
-                    <div>Berpantai: {city.berpantai}</div>
-                    <p/><p/>
-                </div>
-            )
-        });
-        return(
-            <div className="city-list">{ cityList }</div>
+const Cities = (props) => {
+    // console.log(this.props);
+    // const {nama, luas, berpantai} = this.props;
+    const {cities} = props;
+    const cityList = cities.map(city => {
+        return (
+            <div className="city" key={city.id}>
+                <div>Nama: {city.nama}</div>
+                <div>Luas: {city.luas}</div>
+                <div>Berpantai: {city.berpantai}</div>
+                <p/><p/>
+            </div>
         )
-    }
+    });
+    return(
+        <div className="city-list">{ cityList }</div>
+    )
 }
 
 export default Cities
